4230e840e6d4551e25af170a0fafa742bdf63fa8
sonia <sonia.deca@student.upt.ro>
2020-05-26 10:21:57 +0300

    SGC-32 Crearea unei casute de Log In

--- src/main/java/JavaFxMainClass.java
+++ src/main/java/JavaFxMainClass.java
@@ -1,5 +1,10 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class JavaFxMainClass extends Application {
 
@@ -8,7 +13,10 @@ public class JavaFxMainClass extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+        primaryStage.setTitle("Sistem Gestionare Cafenea");
+        primaryStage.setScene(new Scene(root,500,300));
+        primaryStage.show();
     }
 }
