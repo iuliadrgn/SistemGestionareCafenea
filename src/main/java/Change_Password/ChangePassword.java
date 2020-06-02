a138f8ace9ced5f99adbcc182cd9fc95254b0e13
sonia <sonia.deca@student.upt.ro>
2020-06-02 23:30:05 +0300

    SGC-46 crearea unul mesaj de eroare pentru introducerea datelor actuale gresite, sau neintroducere parola

--- src/main/java/Change_Password/ChangePassword.java
+++ src/main/java/Change_Password/ChangePassword.java
@@ -1,11 +1,9 @@
 package Change_Password;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-
-import java.io.IOException;
 
 public class ChangePassword {
     @FXML
@@ -13,12 +11,24 @@ public class ChangePassword {
     @FXML
     public TextField UsernameField;
     @FXML
-    public PasswordField OldPasswordField;
+    public javafx.scene.control.PasswordField OldPasswordField;
     @FXML
-    public PasswordField NewPasswordField;
+    public javafx.scene.control.PasswordField NewPasswordField;
 
     public void Change(ActionEvent actionEvent)  {
-        return;
+        String username = UsernameField.getText();
+        String oldpassword = OldPasswordField.getText();
+        String newpassword = NewPasswordField.getText();
+        if (username == null || username.isEmpty()) {
+            ChangeMessage.setText("Invalid e-mail");
+        } if (oldpassword == null || oldpassword.isEmpty()) {
+            ChangeMessage.setText("Invalid password");
         }
-
+        if (newpassword == null || newpassword.isEmpty()) {
+            ChangeMessage.setText("Introduce password");
+        }
+        else if(username.equals("admin")||username.equals("angajat")||username.equals("furnizor"))
+            return;
+        else ChangeMessage.setText("Invalid Credentials");
+    }
 }
