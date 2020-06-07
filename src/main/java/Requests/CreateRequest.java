a2689513343ccb5fcbcf4f475a25322ce0678e82
sonia <sonia.deca@student.upt.ro>
2020-06-07 16:33:18 +0300

    SGC-56 Creare textbox pentru produs,nr. bucati si choicebox "urgent-da/nu"

--- src/main/java/Requests/CreateRequest.java
+++ src/main/java/Requests/CreateRequest.java
@@ -1,4 +1,16 @@
 package Requests;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class CreateRequest {
+    public TextField NameField;
+    public TextField NumberField;
+    public ChoiceBox UrgentField;
+    public Text RequestMessage;
+
+    public void Okay(ActionEvent actionEvent) {
+    }
 }
