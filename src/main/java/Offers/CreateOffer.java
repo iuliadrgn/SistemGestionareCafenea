3a13e88863de3ac27ea6b710a2690131f47df1b0
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-10 00:49:17 +0300

    SGC-93 Creare Textbox pentru produs, numar bucati, pret pe bucata si un choicebox: fresh/inghetat

--- src/main/java/Offers/CreateOffer.java
+++ src/main/java/Offers/CreateOffer.java
@@ -1,4 +1,11 @@
 package Offers;
 
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
 public class CreateOffer {
+    public ChoiceBox state;
+    public TextField price;
+    public TextField number;
+    public TextField product;
 }
