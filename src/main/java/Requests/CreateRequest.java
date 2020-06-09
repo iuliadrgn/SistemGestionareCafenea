67116ba1892530be33bf5e18fdfbfa2bc8308ba4
sonia <sonia.deca@student.upt.ro>
2020-06-09 18:01:29 +0300

    SGC-85 Creare buton "back"

--- src/main/java/Requests/CreateRequest.java
+++ src/main/java/Requests/CreateRequest.java
@@ -1,9 +1,15 @@
 package Requests;
 
 import exceptions.RequestAlreadyExistsException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import services.RequestService;
 
 import java.io.IOException;
@@ -45,4 +51,18 @@ public class CreateRequest {
 
 
     }
-}
+
+    public void Back(ActionEvent actionEvent) {
+        try{
+            Parent log_in= FXMLLoader.load(getClass().getResource("/HomeAngajat.fxml"));
+            Stage stage= new Stage();
+            stage.setTitle("Sistem Gestionare Cafenea");
+            Scene scene=new Scene(log_in,700,500);
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    }
