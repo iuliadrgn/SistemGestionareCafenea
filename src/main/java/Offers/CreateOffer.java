8e11a89d8b4eb3818671c6ff630203805d3beb74
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-10 00:59:31 +0300

    SGC-100 creare buton de back

--- src/main/java/Offers/CreateOffer.java
+++ src/main/java/Offers/CreateOffer.java
@@ -1,8 +1,15 @@
 package Offers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateOffer {
     public ChoiceBox state;
@@ -13,4 +20,19 @@ public class CreateOffer {
     public void ok(ActionEvent actionEvent) {
         return;
     }
+
+    public void Back(ActionEvent actionEvent) {
+        try{
+            Parent log_in= FXMLLoader.load(getClass().getResource("/HomeFurnizor.fxml"));
+            Stage stage= new Stage();
+            stage.setTitle("Sistem Gestionare Cafenea");
+            Scene scene=new Scene(log_in,700,500);
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
