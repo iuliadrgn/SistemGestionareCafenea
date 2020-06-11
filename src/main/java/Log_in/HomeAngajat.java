09ac86b6f94e102242450a74151a6af918bc4911
Corina Calin <calin_corina64@yahoo.com>
2020-06-11 22:35:44 +0300

    SGC-111 Legarea paginii see request de HomeAngajat

--- src/main/java/Log_in/HomeAngajat.java
+++ src/main/java/Log_in/HomeAngajat.java
@@ -52,5 +52,15 @@ public class HomeAngajat {
     }
 
     public void SeeOffers(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("SeeOffersAngajat.fxml")));
+            stage.setTitle("Sistem Gestionare Cafenea");
+            stage.setScene(new Scene(root, 500, 300));
+            stage.show();
+            ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
