926118dfbce03a5687cf94bbd0c91964a23e55c9
Corina Calin <calin_corina64@yahoo.com>
2020-06-11 21:28:14 +0300

    SGC-110 Crearea unui buton "Back"

---
+++ src/main/java/Offers/SeeOffersAngajat.java
@@ -0,0 +1,26 @@
+package Offers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SeeOffersAngajat {
+    public void Back(ActionEvent actionEvent) {
+        try{
+            Parent log_in= FXMLLoader.load(getClass().getResource("/HomeAngajat.fxml"));
+            Stage stage= new Stage();
+            stage.setTitle("Sistem Gestionare Cafenea");
+            Scene scene=new Scene(log_in,700,500);
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
