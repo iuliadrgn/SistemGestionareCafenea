01a05cb4e48db9d5e24b42919f0039c61fdbbc0a
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-12 23:24:48 +0300

    SGC-16 crearea unui buton "Adaugare" pentru adaugarea produselor

--- src/main/java/Requests/SeeRequestsAngajat.java
+++ src/main/java/Requests/SeeRequestsAngajat.java
@@ -109,4 +109,17 @@ public class SeeRequestsAngajat implements Initializable {
 
         TableView.getItems().removeAll(TableView.getSelectionModel().getSelectedItem());
     }
+
+    public void Add(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("AddRequest.fxml")));
+            stage.setTitle("Sistem Gestionare Cafenea");
+            stage.setScene(new Scene(root, 500, 300));
+            stage.show();
+            ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
