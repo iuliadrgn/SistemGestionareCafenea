e28cda98ad1a1822c7620d1f6cd99ce1a48f34e2
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-12 22:46:17 +0300

    SGC-116 afisarea cererilor in tabel

--- src/main/java/Requests/SeeRequestsAngajat.java
+++ src/main/java/Requests/SeeRequestsAngajat.java
@@ -1,19 +1,39 @@
 package Requests;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exceptions.CouldNotWriteOffersException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import models.Offer;
 import models.Request;
+import org.apache.commons.io.FileUtils;
+import services.FileSystemService;
+import services.OfferService;
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class SeeRequestsAngajat {
+public class SeeRequestsAngajat implements Initializable {
+    private static List<Request> requests;
+    private static final Path OFFER_PATH = FileSystemService.getPathToFile("req", "requests.json");
+
     @FXML
     public javafx.scene.control.TableView<Request> TableView;
     @FXML
@@ -36,5 +56,38 @@ public class SeeRequestsAngajat {
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        number.setCellValueFactory(new PropertyValueFactory<>("number"));
+        urgent.setCellValueFactory(new PropertyValueFactory<>("urgent"));
+
+
+        try {
+            TableView.setItems(getPeople());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private ObservableList<Request> getPeople() throws IOException {
+        ObservableList<Request> cereri= FXCollections.observableArrayList();
+        if (!Files.exists(OFFER_PATH)) {
+            FileUtils.copyURLToFile(Objects.requireNonNull(OfferService.class.getClassLoader().getResource("requests.json")), OFFER_PATH.toFile());
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        requests = objectMapper.readValue(OFFER_PATH.toFile(), new TypeReference<List<Request>>() {
+        });
+
+        cereri.addAll(requests);
+        return cereri;
+
     }
 }
