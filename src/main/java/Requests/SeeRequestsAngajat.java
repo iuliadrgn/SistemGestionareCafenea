dd7df17c6f2e4fe3aa607a95292af5ea05122062
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-12 22:33:18 +0300

    SGC-92 crearea unui tabel in care sa se afiseze cererile de produse

--- src/main/java/Requests/SeeRequestsAngajat.java
+++ src/main/java/Requests/SeeRequestsAngajat.java
@@ -1,15 +1,28 @@
 package Requests;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import models.Request;
 
 import java.io.IOException;
 
 public class SeeRequestsAngajat {
+    @FXML
+    public javafx.scene.control.TableView<Request> TableView;
+    @FXML
+    public TableColumn<Request,String> name;
+    @FXML
+    public TableColumn<Request,String> number;
+    @FXML
+    public TableColumn<Request,String> urgent;
+
 
     public void Back(ActionEvent actionEvent) {
         try{
