eab9142d00a8370dfa4f189849a465eb8d66a3da
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-14 01:12:16 +0300

    SGC-106 creare buton "back"

--- src/main/java/Contracts/CreateContract.java
+++ src/main/java/Contracts/CreateContract.java
@@ -1,8 +1,30 @@
 package Contracts;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateContract {
     public void Ok(ActionEvent actionEvent) {
     }
+
+    public void Back(ActionEvent actionEvent) {
+        try{
+            Parent log_in= FXMLLoader.load(getClass().getResource("/SeeOffersAngajat.fxml"));
+            Stage stage= new Stage();
+            stage.setTitle("Sistem Gestionare Cafenea");
+            Scene scene=new Scene(log_in,600,400);
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
