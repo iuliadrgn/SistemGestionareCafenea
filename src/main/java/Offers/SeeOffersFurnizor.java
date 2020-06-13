7f12fd3b5981bddfe4e3408d630f7b1ed6a27a64
Corina Calin <calin_corina64@yahoo.com>
2020-06-13 23:39:49 +0300

    SGC-19 Crearea unui buton "Add"

--- src/main/java/Offers/SeeOffersFurnizor.java
+++ src/main/java/Offers/SeeOffersFurnizor.java
@@ -135,4 +135,17 @@ public class SeeOffersFurnizor implements Initializable {
         Offer ofr=TableView.getSelectionModel().getSelectedItem();
         ofr.setState(requestStringCellEditEvent.getNewValue());
     }
+
+    public void Add(ActionEvent actionEvent) {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("AddOffer.fxml")));
+            stage.setTitle("Sistem Gestionare Cafenea");
+            stage.setScene(new Scene(root, 500, 300));
+            stage.show();
+            ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
