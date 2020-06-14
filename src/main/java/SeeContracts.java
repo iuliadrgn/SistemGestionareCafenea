dc71664110337d81fbc3af0e52424085fd80cc98
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-15 01:22:59 +0300

    SGC-42 crearea unui tabel pentru afisarea contractelor

--- src/main/java/SeeContracts.java
+++ src/main/java/SeeContracts.java
@@ -3,11 +3,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
+import models.Contract;
 
 import java.io.IOException;
 
 public class SeeContracts {
+    public javafx.scene.control.TableView<Contract> TableView;
+    public TableColumn<Contract,String> product;
+    public TableColumn<Contract,String> price;
+    public TableColumn<Contract,String> number;
+    public TableColumn<Contract,String> state;
+    public TableColumn<Contract,String> numef;
+    public TableColumn<Contract,String> data;
+
     public void Back(ActionEvent actionEvent) {
         try{
             Parent log_in= FXMLLoader.load(getClass().getResource("/HomeAdministrator.fxml"));
