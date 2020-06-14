a3979ab382fb245ed5c43a7c0fd9f13e1ae3d8a8
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-15 01:10:44 +0300

    SGC-132 crearea unui buton de back

--- src/main/java/SeeContracts.java
+++ src/main/java/SeeContracts.java
@@ -1,2 +1,24 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SeeContracts {
+    public void Back(ActionEvent actionEvent) {
+        try{
+            Parent log_in= FXMLLoader.load(getClass().getResource("/HomeAdministrator.fxml"));
+            Stage stage= new Stage();
+            stage.setTitle("Sistem Gestionare Cafenea");
+            Scene scene=new Scene(log_in,700,500);
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
