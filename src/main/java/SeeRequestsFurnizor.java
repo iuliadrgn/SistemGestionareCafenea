9ad64ccc831a60f9623c227b78419bc98328444d
Corina Calin <calin_corina64@yahoo.com>
2020-06-14 21:32:51 +0300

    SGC-127 Crearea unui buton "Back"

--- src/main/java/SeeRequestsFurnizor.java
+++ src/main/java/SeeRequestsFurnizor.java
@@ -1,3 +1,25 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SeeRequestsFurnizor {
 
+    public void Back(ActionEvent actionEvent) {
+        try{
+            Parent log_in= FXMLLoader.load(getClass().getResource("/HomeFurnizor.fxml"));
+            Stage stage= new Stage();
+            stage.setTitle("Sistem Gestionare Cafenea");
+            Scene scene=new Scene(log_in,700,500);
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
