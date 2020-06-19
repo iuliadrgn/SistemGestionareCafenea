a368060deed011b085ee8b1582a928d14b594f1b
sonia <sonia.deca@student.upt.ro>
2020-06-19 16:19:40 +0300

    SGC-139 Add ten users-Test

--- src/test/java/services/UserServiceTest.java
+++ src/test/java/services/UserServiceTest.java
@@ -56,4 +56,20 @@ public class UserServiceTest {
         assertNotNull(UserService.users);
         assertEquals(2, UserService.users.size());
     }
+    @Test
+    public void testAddTenUsers() throws Exception {
+        UserService.loadUsersFromFile();
+        UserService.addUser("test1", "testPass1", "test2users");
+        UserService.addUser("test2", "testPass2", "test2users");
+        UserService.addUser("test3", "testPass3", "test2users");
+        UserService.addUser("test4", "testPass4", "test2users");
+        UserService.addUser("test5", "testPass5", "test2users");
+        UserService.addUser("test6", "testPass6", "test2users");
+        UserService.addUser("test7", "testPass7", "test2users");
+        UserService.addUser("test8", "testPass8", "test2users");
+        UserService.addUser("test9", "testPass9", "test2users");
+        UserService.addUser("test10", "testPass10", "test2users");
+        assertNotNull(UserService.users);
+        assertEquals(10, UserService.users.size());
+    }
 }
