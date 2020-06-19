370fac1c4dca8692a35c8c540176adc94c5b91bc
sonia <sonia.deca@student.upt.ro>
2020-06-19 19:19:46 +0300

    SGC-157 Load Contracts from file-Test

---
+++ src/test/java/services/ContractServiceTest.java
@@ -0,0 +1,45 @@
+package services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exceptions.ContractAlreadyExistsException;
+import exceptions.RequestAlreadyExistsException;
+import models.Contract;
+import models.Request;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ContractServiceTest {
+    @BeforeClass
+    public static void setupClass() {
+        FileSystemService.APPLICATION_FOLDER = ".FisOnline";
+        FileSystemService.initApplicationHomeDirIfNeeded();
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomePath().toFile());
+    }
+
+    @Test
+    public void testCopyDefaultFileIfNotExists() throws Exception {
+        ContractService.loadContractsFromFile();
+        assertTrue(Files.exists(ContractService.CONTRACT_PATH));
+    }
+
+    @Test
+    public void testLoadContractsFromFile() throws Exception {
+        ContractService.loadContractsFromFile();
+        assertNotNull(ContractService.contracts);
+        Assert.assertEquals(0, ContractService.contracts.size());
+    }
+}
