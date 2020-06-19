418dd6154ae4d14de67f567b86aa25a66d7f8532
sonia <sonia.deca@student.upt.ro>
2020-06-19 19:29:21 +0300

    SGC-162 Persist two contracts-Test

--- src/test/java/services/ContractServiceTest.java
+++ src/test/java/services/ContractServiceTest.java
@@ -74,5 +74,16 @@ public class ContractServiceTest {
         assertNotNull(ContractService.contracts);
         assertEquals(1, contracts.size());
     }
+    @Test
+    public void testAddTwoContractsArePersisted() throws Exception {
+        ContractService.loadContractsFromFile();
+        ContractService.addContract("test1", "testPass1", "test1user","test1","Test1","11/11/1111");
+        ContractService.addContract("test2", "testPass2", "test2user","test1","Test1","11/11/1111");
+        List<Contract> contracts= new ObjectMapper().readValue(ContractService.CONTRACT_PATH.toFile(), new TypeReference<List<Contract>>() {
+        });
+        assertNotNull(ContractService.contracts);
+        assertEquals(2, contracts.size());
+    }
+
 
 }
