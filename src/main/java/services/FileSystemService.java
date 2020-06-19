e1d0a2419b8703be8932728dd1ae6e0b0874c653
sonia <sonia.deca@student.upt.ro>
2020-06-19 16:06:05 +0300

    SGC-136 Load Users from file -Test

--- src/main/java/services/FileSystemService.java
+++ src/main/java/services/FileSystemService.java
@@ -1,14 +1,24 @@
 package services;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileSystemService {
-    private static final String APPLICATION_FOLDER = ".FISOnline";
+    public static String APPLICATION_FOLDER = ".FISOnline";
     private static final String USER_FOLDER = System.getProperty("user.home");
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
     public static Path getPathToFile(String... path) {
         return APPLICATION_HOME_PATH.resolve(Paths.get(".", path));
+    }
+
+    public static void initApplicationHomeDirIfNeeded() {
+        if (!Files.exists(getApplicationHomePath()))
+            getApplicationHomePath().toFile().mkdirs();
+    }
+
+    public static Path getApplicationHomePath() {
+        return Paths.get(USER_FOLDER, APPLICATION_FOLDER);
     }
 }
---
+++ src/test/java/services/UserServiceTest.java
@@ -0,0 +1,44 @@
+package services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exceptions.UsernameAlreadyExistsException;
+import models.User;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotEquals;
+
+public class UserServiceTest {
+    @BeforeClass
+    public static void setupClass() {
+        FileSystemService.APPLICATION_FOLDER = ".FisOnline";
+        FileSystemService.initApplicationHomeDirIfNeeded();
+    }
+
+    @Before
+    public void setUp() throws IOException {
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomePath().toFile());
+    }
+
+    @Test
+    public void testCopyDefaultFileIfNotExists() throws Exception {
+        UserService.loadUsersFromFile();
+        assertTrue(Files.exists(UserService.USERS_PATH));
+    }
+
+    @Test
+    public void testLoadUsersFromFile() throws Exception {
+        UserService.loadUsersFromFile();
+        assertNotNull(UserService.users);
+        Assert.assertEquals(0, UserService.users.size());
+    }
+}
