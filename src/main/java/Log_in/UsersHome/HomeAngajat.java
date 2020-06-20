f6e24a25fbbbdbea3c69f774981591767e38cd32
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-20 18:18:21 +0300

    SGC-177 design SeeRequestsAngajat

--- src/main/java/Log_in/UsersHome/HomeAngajat.java
+++ src/main/java/Log_in/UsersHome/HomeAngajat.java
@@ -69,7 +69,7 @@ public class HomeAngajat {
             Stage stage = new Stage();
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("Requests/SeeRequestsAngajat.fxml")));
             stage.setTitle("Sistem Gestionare Cafenea");
-            stage.setScene(new Scene(root, 500, 300));
+            stage.setScene(new Scene(root, 600, 400));
             stage.show();
             ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
         }catch(IOException e){
