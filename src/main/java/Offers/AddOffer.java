d420137ea76c1b5fc1446125021674313a40a607
Iulia Dragan <iuliadragan02@yahoo.com>
2020-06-21 00:22:13 +0300

    SGC-189 ErrorFix1

---
+++ src/main/java/Offers/AddOffer.java
@@ -0,0 +1,82 @@
+package Offers;
+
+import exceptions.Offers.OfferAlreadyExistsException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import services.Offer.OfferService;
+
+import java.io.IOException;
+
+public class AddOffer {
+    public TextField product;
+    public TextField price;
+    public TextField number;
+    public ChoiceBox<String> state;
+    public TextField numef;
+    public Text OfferMessage;
+
+
+    public void initialize() {state.getItems().addAll("Fresh","Frozen");
+    }
+
+    public void ok(ActionEvent actionEvent)  {
+        String prd=product.getText();
+        String prc=price.getText();
+        String nr=number.getText();
+        String num=numef.getText();
+        if(prd==null|| prd.length()==0)
+        {
+            OfferMessage.setText("Complete the product field");
+
+        }
+        else
+        if(prc==null|| prc.length()==0)
+        {
+            OfferMessage.setText("Complete the price field");
+
+        }
+        else
+        if(nr==null||nr.length()==0)
+        {
+            OfferMessage.setText("Complete the number field");
+        }
+        else
+        if(state.getValue()==null)
+        {
+            OfferMessage.setText("Complete the choicebox!");
+
+        }
+        else
+        if(num==null||num.length()==0)
+        {
+            OfferMessage.setText("Complete the numef field");
+        }
+        else  try {
+            OfferService.addOffer(product.getText(),price.getText(), number.getText(), state.getValue(),numef.getText());
+            OfferMessage.setText("Offer created successfully!");
+        } catch (OfferAlreadyExistsException e) {
+            OfferMessage.setText(e.getMessage());
+        }
+    }
+
+    public void Back(ActionEvent actionEvent) {
+        try{
+            Parent log_in= FXMLLoader.load(getClass().getResource("/Offers/SeeOffersFurnizor.fxml"));
+            Stage stage= new Stage();
+            stage.setTitle("Sistem Gestionare Cafenea");
+            Scene scene=new Scene(log_in,600,400);
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
